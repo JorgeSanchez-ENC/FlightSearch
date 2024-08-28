3b9053248f25f14f428e25a9be32bbb073ec70d5
Jorge Sanchez <jorge.siqueiros@encora.com>
2024-08-28 12:38:58 -0600

    Added a service to retreive the access token, as well as a service to perform the request to flight offer search

---
+++ backend/src/main/java/com/flightsearch/backend/Services/AccessTokenService.java
@@ -0,0 +1,33 @@
+package com.flightsearch.backend.Services;
+
+
+import okhttp3.*;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class AccessTokenService {
+    public String getAccessToken( String apiKey, String apiSecret) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        RequestBody formBody = new FormBody.Builder()
+                .add("grant_type", "client_credentials")
+                .add("client_id",apiKey)
+                .add("client_secret",apiSecret)
+                .build();
+
+        Request request = new Request.Builder()
+                .url("https://test.api.amadeus.com/v1/security/oauth2/token")
+                .post(formBody)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            assert response.body() != null;
+            String responseBody = response.body().string();
+            JSONObject jsonObject = new JSONObject(responseBody);
+            return jsonObject.getString("access_token");
+        }
+    }
+}
---
+++ backend/src/main/java/com/flightsearch/backend/Services/SearchService.java
@@ -0,0 +1,61 @@
+package com.flightsearch.backend.Services;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class SearchService {
+    @Autowired
+    AccessTokenService accessTokenService;
+
+    OkHttpClient client = new OkHttpClient();
+    @Value("${apiKey}")
+    String apiKey;
+    @Value("${apiSecret}")
+    String apiSecret;
+
+
+    public SearchService() throws IOException {
+    }
+
+    public String flightOfferSearch(String originLocationCode, String destinationLocationCode,
+        String departureDate, String returnDate, Integer adults, String currencyCode, Boolean nonStop
+    ) throws IOException {
+        String token = accessTokenService.getAccessToken(apiKey,apiSecret);
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://test.api.amadeus.com/v2/shopping/flight-offers").newBuilder()
+                .addQueryParameter("originLocationCode",originLocationCode)
+                .addQueryParameter("destinationLocationCode",destinationLocationCode)
+                .addQueryParameter("departureDate",departureDate)
+                .addQueryParameter("adults",adults.toString())
+                .addQueryParameter("currencyCode", currencyCode )
+                .addQueryParameter("nonStop", nonStop.toString());
+        if(returnDate != null && !returnDate.isEmpty()){
+            urlBuilder.addQueryParameter("returnDate", returnDate);
+        }
+
+        HttpUrl url = urlBuilder.build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("Authorization","Bearer "+ token)
+                .build();
+
+        try(Response response = client.newCall(request).execute()){
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            assert response.body() != null;
+            return response.body().string();
+        }
+
+
+    }
+}
